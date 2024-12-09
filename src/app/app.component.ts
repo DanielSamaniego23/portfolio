269485e368cb6eb1aaf810268fefa15528a0bebf
danisamaniego370 <danisamaniego370@gmail.com>
2024-12-09 19:10:00 +0000

    V1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+
+declare var particlesJS: any; // Declarar particlesJS
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myapp';
+
+  ngOnInit() {
+    particlesJS.load('particles-js', 'assets/particles-config.js', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
